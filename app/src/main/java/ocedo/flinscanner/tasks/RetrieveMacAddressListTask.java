c4811364e5a035ca571a11ea4cd5164fdadef815
Stefan Neidig <stefan.neidig@densio.com>
2015-04-22 13:00:31 +0200

    Change retrieve mac address list task from async task to class with static methods

--- app/src/main/java/ocedo/flinscanner/tasks/RetrieveMacAddressListTask.java
+++ app/src/main/java/ocedo/flinscanner/tasks/RetrieveMacAddressListTask.java
@@ -1,64 +1,82 @@
 package ocedo.flinscanner.tasks;
 
-import android.os.AsyncTask;
-import android.util.Log;
-
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 
 /**
  * Created by stefan on 21.04.15.
  */
-public class RetrieveMacAddressListTask extends AsyncTask<Void, Integer, HashMap<String, String>> {
+public class RetrieveMacAddressListTask {
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        Log.d("TAG", "GOOO");
+    private static final String DEFAULT_MAC = "00:00:00:00:00:00";
+
+    public RetrieveMacAddressListTask() {
     }
 
-    @Override
-    protected void onPostExecute(HashMap<String, String> stringStringHashMap) {
-        super.onPostExecute(stringStringHashMap);
-        Log.d("TAG", "FiNALLY");
-    }
+    public static String getHardwareAddressFromIP(String ip) {
+        String result = DEFAULT_MAC;
 
-    @Override
-    protected HashMap<String, String> doInBackground(Void... voids) {
-        Log.d("TAG", "HJFKSDHJKL");
+        BufferedReader reader = null;
 
-        HashMap<String, String> result = new HashMap<>();
-
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader("/proc/net/arp"));
+        try{
+            reader = new BufferedReader(new FileReader("/proc/net/arp"));
             String line;
-            while ((line = br.readLine()) != null) {
-                Log.d("TAG", line);
-
+            while ((line = reader.readLine()) != null) {
+               // Log.d("TAG", line);
                 String[] splitted = line.split(" +");
-                if (splitted != null && splitted.length >= 4) {
+                if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
                     // Basic sanity check
                     String mac = splitted[3];
                     if (mac.matches("..:..:..:..:..:..")) {
-                        result.put(splitted[0], mac);
+                        result = mac;
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(result.equals(DEFAULT_MAC)) {
+            String alternativeMac = getHardwareAddressFromNetworkInterface(ip);
+            if(!alternativeMac.equals(DEFAULT_MAC)) {
+                result = alternativeMac;
             }
         }
 
-        Log.d("TAG", "Done");
-
         return result;
+    }
+
+    private static String getHardwareAddressFromNetworkInterface(String ip) {
+        try {
+            NetworkInterface network = NetworkInterface.getByInetAddress(InetAddress.getByName(ip));
+
+            if(network != null) {
+                byte[] mac = network.getHardwareAddress();
+
+                StringBuilder builder = new StringBuilder();
+
+                for(int i = 0; i < mac.length; i ++) {
+                    builder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : ""));
+                }
+
+                return builder.toString();
+            }
+
+            return DEFAULT_MAC;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return DEFAULT_MAC;
+        }
+    }
+
+    public static String getVendorFromMacAddress(String macAddress) {
+        return "";
     }
 }
