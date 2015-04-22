98daa3cd704981737fd8b1b7ea2f5a9e8b7adbda
Stefan Neidig <stefan.neidig@densio.com>
2015-04-22 14:31:32 +0200

    Extract gateway before scan and retrieve subnet for search

--- app/src/main/java/ocedo/flinscanner/MainActivity.java
+++ app/src/main/java/ocedo/flinscanner/MainActivity.java
@@ -4,7 +4,7 @@ import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.format.Formatter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,10 +13,15 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 import in.srain.cube.views.ptr.PtrDefaultHandler;
 import in.srain.cube.views.ptr.PtrFrameLayout;
 import in.srain.cube.views.ptr.PtrHandler;
-import ocedo.flinscanner.lists.HostListItem;
 import ocedo.flinscanner.lists.HostListItemAdapter;
 import ocedo.flinscanner.tasks.ScanNetworkTask;
 
@@ -60,20 +65,75 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
+    private void getGateWay(WifiManager manager) {
+        try {
+            for(Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface networkInterface = en.nextElement();
+                String name = networkInterface.getName();
+                String ip = "";
+                String gateWayIP = "";
+                String netmaskIP = "";
+
+                for(Enumeration<InetAddress> addresses = networkInterface.getInetAddresses(); addresses.hasMoreElements(); ) {
+                    InetAddress address = addresses.nextElement();
+                    if(!address.isLoopbackAddress()) {
+                        if(address instanceof Inet6Address) {
+                            continue;
+                        }
+
+                        ip = address.getHostAddress();
+                    }
+                }
+
+                if(manager != null) {
+                    DhcpInfo info = manager.getDhcpInfo();
+
+                    gateWayIP = String.format(
+                            "%d.%d.%d.%d",
+                            (info.gateway & 0xff),
+                            (info.gateway >> 8 & 0xff),
+                            (info.gateway >> 16 & 0xff),
+                            (info.gateway >> 24 & 0xff));
+
+                    netmaskIP = String.format(
+                            "%d.%d.%d.%d",
+                            (info.netmask & 0xff),
+                            (info.netmask >> 8 & 0xff),
+                            (info.netmask >> 16 & 0xff),
+                            (info.netmask >> 24 & 0xff));
+                }
+
+                Log.d("TAG", "Name: " + name + ", IP: " + ip + ", Gateway: " + gateWayIP + ", Subnet: " + netmaskIP);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String integerIpToString(int ip) {
+       return String.format(
+                "%d.%d.%d.%d",
+                (ip & 0xff),
+                (ip >> 8 & 0xff),
+                (ip >> 16 & 0xff),
+                (ip >> 24 & 0xff));
+    }
+
     private void startScanConcurrently() {
         WifiManager manager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         if(manager != null) {
             hostListAdapter.clear();
 
             DhcpInfo dhcpInfo = manager.getDhcpInfo();
+            String gateway = integerIpToString(dhcpInfo.gateway);
 
-            HostListItem item = new HostListItem("Router", Formatter.formatIpAddress(dhcpInfo.gateway));
-            item.setType(HostListItem.HOST_ITEM_TYPE.UNKNOWN);
+            int last = gateway.lastIndexOf(".");
+            String maskedGateway = gateway.substring(0, last+1);
 
-            hostListAdapter.addItem(item);
+            Log.d("TAG", "Searching in " + maskedGateway);
 
-            ScanNetworkTask task = new ScanNetworkTask(hostListAdapter, pullToRefreshContainer, progressBar);
-            task.execute(0, 256);
+            ScanNetworkTask task = new ScanNetworkTask(getApplicationContext(), hostListAdapter, pullToRefreshContainer, progressBar);
+            task.execute(maskedGateway);
 
         } else {
             Toast.makeText(getApplicationContext(), "Could not start scan", Toast.LENGTH_SHORT).show();
